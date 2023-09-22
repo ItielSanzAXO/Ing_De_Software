f52af2ef37df6513c021b1a18c6dc95a0fb75183
ItielSanz /AXO <113627289+ItielSanzAXO@users.noreply.github.com>
2023-09-22 02:35:06 -0600

    Clase Conexión

--- CalculoDeBarras/src/main/java/calculodebarras/CalculoDeBarras.java
+++ CalculoDeBarras/src/main/java/calculodebarras/CalculoDeBarras.java
@@ -4,6 +4,9 @@
 
 package calculodebarras;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author ItielSanz<ItielSanzAXO>
@@ -11,7 +14,28 @@ package calculodebarras;
 public class CalculoDeBarras {
 
     public static void main(String[] args) {
-        Login nv = new Login();
-        nv.setVisible(true);
+        // Obtén una instancia de DatabaseConnection con la cadena de conexión adecuada
+        String connectionString = "jdbc:sqlserver://ingsoftdatabase.database.windows.net:1433;database=IngSoftwareDB;user=admin26@ingsoftdatabase;password=@Axopunk2023.;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        DatabaseConnection dbConnection = DatabaseConnection.getInstance(connectionString);
+
+        // Intenta obtener la conexión a la base de datos
+        Connection connection = dbConnection.getConnection();
+
+        // Verifica si la conexión se estableció con éxito
+        if (connection != null) {
+            System.out.println("Conexión exitosa a la base de datos.");
+            
+            // Aquí puedes realizar operaciones en la base de datos
+            
+            // No olvides cerrar la conexión cuando hayas terminado
+            try {
+                connection.close();
+                System.out.println("Conexión cerrada.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Error al conectar a la base de datos.");
+        }
     }
 }
---
+++ CalculoDeBarras/src/main/java/calculodebarras/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package calculodebarras;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance = null;
+    private Connection connection;
+
+    // Constructor privado para evitar la creación de instancias desde fuera de la clase
+    private DatabaseConnection(String connectionString) {
+        try {
+            connection = DriverManager.getConnection(connectionString);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para obtener la única instancia de DatabaseConnection
+    public static synchronized DatabaseConnection getInstance(String connectionString) {
+        if (instance == null) {
+            instance = new DatabaseConnection(connectionString);
+        }
+        return instance;
+    }
+
+    // Método para obtener la conexión a la base de datos
+    public Connection getConnection() {
+        return connection;
+    }
+}
